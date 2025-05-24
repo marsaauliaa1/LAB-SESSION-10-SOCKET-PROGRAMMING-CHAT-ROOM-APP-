778b217b14bfd55b358e7b3547470c0fe74c1ac2
marsaauliaa1 <aulsa01@gmail.com>
2025-05-24 19:27:35 +0700

    client_1.py

--- client_1.py
+++ client_1.py
@@ -2,7 +2,7 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket.connect(('172.16.49.109', 12345))
+client_socket.connect(('192.168.56.1', 12345))
 
 client_socket.send("Halo kenalan yuk!".encode())
 
