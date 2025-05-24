27ed1e0169c05d68c87dfb6b3d5ad1facea16f3c
marsaauliaa1 <aulsa01@gmail.com>
2025-05-24 19:29:55 +0700

    client_1.py

--- client_1.py
+++ client_1.py
@@ -4,7 +4,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect(('192.168.56.1', 12345))
 
-client_socket.send("Halo kenalan yuk!".encode())
+client_socket.send("Halo client_2!".encode())
 
 response = client_socket.recv(1024).decode()
 print(f"Balasan dari server: {response}")
